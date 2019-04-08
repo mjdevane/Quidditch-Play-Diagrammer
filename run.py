698541d77e09ad8954cca6e85c481ac8ed7760a6
Skar-Lath <hyper123dog@yahoo.com>
2019-04-08 19:12:49 -0400

    Revert "remove unneccessary flask stuff"
    This reverts commit 0561bb2754c568fe33cb2b1c946beac7408c8d67.

--- run.py
+++ run.py
@@ -8,5 +8,10 @@ def index():
     return render_template('base.html')
 
 
+@app.route('/<path:path>')
+def base_static(path):
+    return send_file(os.path.join(app.root_path, path))
+
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8016, debug=False)
