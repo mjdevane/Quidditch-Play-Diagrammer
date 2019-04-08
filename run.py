0561bb2754c568fe33cb2b1c946beac7408c8d67
Skar-Lath <hyper123dog@yahoo.com>
2019-04-08 19:09:31 -0400

    remove unneccessary flask stuff
    This mostly stops Flask from yelling at me in the command window

--- run.py
+++ run.py
@@ -8,10 +8,5 @@ def index():
     return render_template('base.html')
 
 
-@app.route('/<path:path>')
-def base_static(path):
-    return send_file(os.path.join(app.root_path, path))
-
-
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8016, debug=False)
